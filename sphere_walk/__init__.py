712da10ca09d26d5f672122681a6969fad0f095a
Romain Simon <93144534+romainljsimon@users.noreply.github.com>
2024-10-31 17:30:29 +0100

    change __init__.py

---
+++ sphere_walk/__init__.py
@@ -0,0 +1,6 @@
+from .brownian_motion import PointWalk, LineWalk
+from .z_functions import ZFunctions
+from .random_walk import XYZWalk
+
+
+__all__ = ['PointWalk', 'LineWalk', 'ZFunctions', 'XYZWalk']
